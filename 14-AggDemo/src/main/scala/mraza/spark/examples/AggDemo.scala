595162bdd6cfd09b5aaca46993708bf31e560c1b
mraza <mraza@adobe.com>
2025-05-18 20:58:56 +0530

    Grouping Aggregation

--- 14-AggDemo/src/main/scala/mraza/spark/examples/AggDemo.scala
+++ 14-AggDemo/src/main/scala/mraza/spark/examples/AggDemo.scala
@@ -20,39 +20,41 @@ object AggDemo extends Serializable {
       .option("inferSchema", "true")
       .load("data/invoices.csv")
 
-        invoiceDF.select(
-          count("*").as("Count *"),
-          sum("Quantity").as("TotalQuantity"),
-          avg("UnitPrice").as("AvgPrice"),
-          countDistinct("InvoiceNo").as("CountDistinct")
-        ).show()
+    invoiceDF.
 
-
-        invoiceDF.selectExpr(
-          "count(1) as `count 1`",
-          "count(StockCode) as `count field`",
-          "sum(Quantity) as TotalQuantity",
-          "avg(UnitPrice) as AvgPrice"
-        ).show()
-
-        invoiceDF.createOrReplaceTempView("sales")
-        val summarySQL = spark.sql(
-          """
-            |SELECT Country, InvoiceNo,
-            | sum(Quantity) as TotalQuantity,
-            | round(sum(Quantity*UnitPrice),2) as InvoiceValue
-            | FROM sales
-            | GROUP BY Country, InvoiceNo
-            |""".stripMargin)
-
-        summarySQL.show()
-
-    val summaryDF = invoiceDF.groupBy("Country", "InvoiceNo")
-      .agg(sum("Quantity").as("TotalQuantity"),
-        round(sum(expr("Quantity*UnitPrice")), 2).as("InvoiceValue"),
-        expr("round(sum(Quantity*UnitPrice),2) as InvoiceValueExpr")
-      )
-    summaryDF.show()
+//        invoiceDF.select(
+//          count("*").as("Count *"),
+//          sum("Quantity").as("TotalQuantity"),
+//          avg("UnitPrice").as("AvgPrice"),
+//          countDistinct("InvoiceNo").as("CountDistinct")
+//        ).show()
+//
+//
+//        invoiceDF.selectExpr(
+//          "count(1) as `count 1`",
+//          "count(StockCode) as `count field`",
+//          "sum(Quantity) as TotalQuantity",
+//          "avg(UnitPrice) as AvgPrice"
+//        ).show()
+//
+//        invoiceDF.createOrReplaceTempView("sales")
+//        val summarySQL = spark.sql(
+//          """
+//            |SELECT Country, InvoiceNo,
+//            | sum(Quantity) as TotalQuantity,
+//            | round(sum(Quantity*UnitPrice),2) as InvoiceValue
+//            | FROM sales
+//            | GROUP BY Country, InvoiceNo
+//            |""".stripMargin)
+//
+//        summarySQL.show()
+//
+//    val summaryDF = invoiceDF.groupBy("Country", "InvoiceNo")
+//      .agg(sum("Quantity").as("TotalQuantity"),
+//        round(sum(expr("Quantity*UnitPrice")), 2).as("InvoiceValue"),
+//        expr("round(sum(Quantity*UnitPrice),2) as InvoiceValueExpr")
+//      )
+//    summaryDF.show()
 
   }
 
